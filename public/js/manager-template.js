901c602a968bee6745e24fc49e25eca717011144
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-09-11 18:34:01 -0400

    adding manger html and styles

--- public/js/manager-template.js
+++ public/js/manager-template.js
@@ -1,2 +1,5 @@
-let inputText = `<div class="input-text">{%INPUT%}</div>`;
+let categoryDataTable = `<table class="category-data-table"><thead>{%THEAD%}</thead><tbody>{%TBODY%}</tbody></table>`;
+let categoryTableHeadData = `<th class="category-data-table-data">{%COLUMNNAME%}</th>`;
+let categoryTableRow = `<tr>{%TROWDATA%}</tr>`;
+let categoryTableData = `<td class="category-data-table-data"><div class="input-text" data-column="{%COLUMNNAME%}">{%DATA%}</div></td>`;
 let dataInput = `<input class="data-input" type="text" value="1" />`;
--- public/js/manager.js
+++ public/js/manager.js
@@ -1,22 +1,89 @@
+const columnNameFormatter = columnName => {
+  let captColumnName = columnName.toUpperCase();
+  let indexArr = new Array();
+  for (let i = 0; i < captColumnName.length; i++) {
+    if (columnName[i] === captColumnName[i]) indexArr.push(i);
+  }
+  let formattedName = new String();
+  let initIndex = 0;
+  if (indexArr.length !== 0) {
+    indexArr.map((index, i) => {
+      formattedName += `${columnName.slice(initIndex, index)} `;
+      initIndex = index;
+    });
+    return (
+      formattedName[0].toUpperCase() +
+      formattedName.slice(1) +
+      columnName.slice(initIndex)
+    );
+  } else return columnName[0].toUpperCase() + columnName.slice(1);
+};
+
+const displayByCategory = async category => {
+  let res = await fetch(`./api/${category}`);
+  categoryData = await res.json();
+  let tHeadHtml = new String();
+  let tBodyHtml = new String();
+  categoryData.map((data, i) => {
+    let tRowHtml = new String();
+    for (let columnName in data) {
+      if (columnName !== "createdAt" && columnName !== "updatedAt") {
+        if (i === 0)
+          tHeadHtml += categoryTableHeadData.replace(
+            "{%COLUMNNAME%}",
+            columnName === "id" ? "No" : columnNameFormatter(columnName)
+          );
+        let tRowData = categoryTableData.replace("{%COLUMNNAME%}", columnName);
+        tRowData = tRowData.replace(
+          "{%DATA%}",
+          data[columnName] === null ? "-" : data[columnName]
+        );
+        tRowHtml += tRowData;
+      }
+    }
+    tBodyHtml += categoryTableRow.replace("{%TROWDATA%}", tRowHtml);
+  });
+  let tableInnerHtml = categoryDataTable.replace("{%THEAD%}", tHeadHtml);
+  tableInnerHtml = tableInnerHtml.replace("{%TBODY%}", tBodyHtml);
+  document.querySelector(
+    ".category-details-wrapper"
+  ).innerHTML = tableInnerHtml;
+};
+
+// displayByCategory("menus");
+displayByCategory("customers");
+// displayByCategory("tables");
+// displayByCategory("reservations");
+// displayByCategory("informations");
+
+document.querySelectorAll(".category").forEach(categoryBtn =>
+  categoryBtn.addEventListener("click", e => {
+    displayByCategory(e.target.dataset.category);
+  })
+);
+
 document.querySelectorAll(".input-text").forEach(function(input) {
   input.addEventListener("dblclick", function(e) {
     e.target.innerHTML = dataInput;
     const insertHtml = () => {
       let parentElement = document.querySelector(".data-input").parentElement;
-      console.log(parentElement);
-      if (parentElement)
+      console.log(parentElement !== null);
+      if (parentElement !== null)
         parentElement.innerHTML = inputText.replace("{%INPUT%}", 1);
     };
     document.addEventListener("click", function(e) {
       if (!e.target.closest(".data-input")) {
         insertHtml();
+        removeClickEvent();
       }
+      // e.target.innerHTML = inputText;
+    });
+    const removeClickEvent = () =>
       document.removeEventListener("click", function(e) {
         if (!e.target.closest(".data-input")) {
           insertHtml();
+          removeClickEvent();
         }
       });
-      // e.target.innerHTML = inputText;
-    });
   });
 });
